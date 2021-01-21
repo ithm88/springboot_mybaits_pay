862c29307b785e814ba66871b42df546d4aa640d
ithm88 <59330222+ithm88@users.noreply.github.com>
2021-01-21 14:20:40 +0800

    Update SMS.java

--- src/main/java/com/item/itemsms/SMS.java
+++ src/main/java/com/item/itemsms/SMS.java
@@ -19,8 +19,8 @@ public class SMS {
         final String product = "Dysmsapi";//短信API产品名称（短信产品名固定，无需修改）
         final String domain = "dysmsapi.aliyuncs.com";//短信API产品域名（接口地址固定，无需修改）
 //替换成你的AK
-        final String accessKeyId = "LTAI4G5hgksBL31XhAsVPW7x";//你的accessKeyId,参考本文档步骤2
-        final String accessKeySecret = "isgeQExMSZ7KJufGneHp5FDxHvBoGn";//你的accessKeySecret，参考本文档步骤2
+        final String accessKeyId = "ak";//你的accessKeyId,参考本文档步骤2
+        final String accessKeySecret = "sk";//你的accessKeySecret，参考本文档步骤2
 //初始化ascClient,暂时不支持多region（请勿修改）
         IClientProfile profile = DefaultProfile.getProfile("cn-hangzhou", accessKeyId,
                 accessKeySecret);
@@ -38,9 +38,9 @@ public class SMS {
         //必填:待发送手机号。支持以逗号分隔的形式进行批量调用，批量上限为1000个手机号码,批量调用相对于单条调用及时性稍有延迟,验证码类型的短信推荐使用单条调用的方式；发送国际/港澳台消息时，接收号码格式为国际区号+号码，如“85200000000”
         request.setPhoneNumbers("13923815937");
         //必填:短信签名-可在短信控制台中找到
-        request.setSignName("美邦播客");
+        request.setSignName("签名");
         //必填:短信模板-可在短信控制台中找到，发送国际/港澳台消息时，请使用国际/港澳台短信模版
-        request.setTemplateCode("SMS_181500874");
+        request.setTemplateCode("签名模板");
         //可选:模板中的变量替换JSON串,如模板内容为"亲爱的${name},您的验证码为${code}"时,此处的值为
         //友情提示:如果JSON中需要带换行符,请参照标准的JSON协议对换行符的要求,比如短信内容中包含\r\n的情况在JSON中需要表示成\\r\\n,否则会导致JSON在服务端解析失败
 //参考：request.setTemplateParam("{\"变量1\":\"值1\",\"变量2\":\"值2\",\"变量3\":\"值3\"}")
